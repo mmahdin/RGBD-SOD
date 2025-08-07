6755bba90f2773296c195db38b1fd26d9c75dec1
mmahdin <mahdi9roozi@gmail.com>
2025-08-07 19:31:27 +0330

    use evaluate to get all metrics

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+import torch
+import torch.nn as nn
+import argparse
+import os.path as osp
+import os
+from evaluator import Eval_thread
+from dataloader import EvalDataset
+# from concurrent.futures import ThreadPoolExecutor
+
 def clip_gradient(optimizer, grad_clip):
     for group in optimizer.param_groups:
         for param in group['params']:
@@ -11,3 +20,42 @@ def adjust_lr(optimizer, init_lr, epoch, decay_rate=0.1, decay_epoch=30):
         param_group['lr'] = decay*init_lr
         lr=param_group['lr']
     return lr
+
+
+def main(cfg):
+    root_dir = cfg.root_dir
+    if cfg.save_dir is not None:
+        output_dir = cfg.save_dir
+    else:
+        output_dir = root_dir
+    gt_dir = osp.join(root_dir, 'gt')
+    pred_dir = osp.join(root_dir, 'pred')
+    if cfg.methods is None:
+        method_names = os.listdir(pred_dir)
+    else:
+        method_names = cfg.methods.split(' ')
+    if cfg.datasets is None:
+        dataset_names = os.listdir(gt_dir)
+    else:
+        dataset_names = cfg.datasets.split(' ')
+    
+    threads = []
+    for dataset in dataset_names:
+        for method in method_names:
+            loader = EvalDataset(osp.join(pred_dir, method, dataset), osp.join(gt_dir, dataset))
+            thread = Eval_thread(loader, method, dataset, output_dir, cfg.cuda)
+            threads.append(thread)
+    for thread in threads:
+        print(thread.run())
+
+
+def evaluate():
+    class Config:
+        methods = None
+        datasets = None
+        root_dir = "./"
+        save_dir = None
+        cuda = True
+
+    config = Config()
+    main(config)
